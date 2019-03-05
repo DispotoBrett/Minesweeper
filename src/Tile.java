7d29d9ed2498ad4be30b6d698a057ec92de867a7
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2019-03-04 22:13:58 -0800

    Updated class level Javadoc

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,10 @@
-
+/**
+ * Tile represents a Minesweeper square, which may or may not contain a mine.
+ *
+ * @author Jordan Conragan
+ * @author Brett Dispoto
+ * @author Patrick Silvestre
+ */
 public final class Tile 
 {
 	//Code goes here
