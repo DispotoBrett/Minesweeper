4d73339dc710e88073386bda6f2cebf324f9c487
DispotoBrett <dispotobrett@gmail.com>
2019-03-06 12:29:33 -0800

    added accessor method and fields and constructor for tile

--- src/Tile.java
+++ src/Tile.java
@@ -7,5 +7,48 @@
  */
 public final class Tile 
 {
-	//Code goes here
+	//-------------------------Public Interface-----------------------
+	
+	/**
+	 * Constructs a new Tile object.
+	 * @param isMine denotes whether a tile has an underlying mine.
+	 */
+	public Tile(boolean isMine)
+	{
+		this.isMine = isMine;
+		isRevealed = false;
+		isFlagged = false;
+	}
+	
+	/**
+	 * Accessor to determine is a tile has an underlying mine.
+	 * @return isMine denotes if this tile is a mine.
+	 */
+	public boolean isMine()
+	{
+		return isMine;
+	}
+	
+	/**
+	 * Accessor to determine is a tile has been revealed.
+	 * @return isMine denotes if this tile has been revealed.
+	 */
+	public boolean isRevealed()
+	{
+		return isRevealed;
+	}
+	
+	/**
+	 * Accessor to determine is the tile has been flagged.
+	 * @return isMine denotes if this tile has been flagged.
+	 */
+	public boolean isFlagged()
+	{
+		return isFlagged;
+	}
+	//-------------------------Private Fields/ Methods------------------
+	
+	private final boolean isMine;
+	private boolean isRevealed;
+	private boolean isFlagged;
 }
