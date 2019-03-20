ed2bfe8c73473309be5cd1ace99f133a760172aa
JVP15 <46200499+JVP15@users.noreply.github.com>
2019-03-19 18:14:44 -0700

    Updated Explosion.java
    The animation starts off linear, but then it expands in an exponential fashion.

--- src/edu/sjsu/cs/cs151/minesweeper/ui/Explosion.java
+++ src/edu/sjsu/cs/cs151/minesweeper/ui/Explosion.java
@@ -3,30 +3,47 @@ package edu.sjsu.cs.cs151.minesweeper.ui;
 import javax.swing.*;
 import java.awt.geom.*;
 import java.awt.*;
+import java.awt.Color;
 
 public class Explosion extends JComponent implements Explodable
 {
 	private int width = 10;
 	private int height = 10;
 	private int x, y;
+	private double count;
 
 	public Explosion(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
+		count = 0;
 	}
 
 	public void explode()
 	{
-		if(width % 2 == 1)
-			x--;
-		if(width % 3 == 1)
-			y--;
+		if(count < 10)
+		{
+			if(width % 2 == 1)
+				x--;
+			if(width % 3 == 1)
+				y--;
 
-		height++;
-		width++; 
+			height++;
+			width++; 
+			count = count + .1;
+		}
+	
+		else
+		{
+			int delta = (int) ( 1.0/100 * Math.pow(count, 3) );
+			height += delta * 2;
+			width += delta * 2;
+			x -= delta;
+			y -= delta;
+			count++;
+		}
 	}
-
+	
 	public int getIconWidth(){return width;}
 	public int getIconHeight(){return height;}
 	
