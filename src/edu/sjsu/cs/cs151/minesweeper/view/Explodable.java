a6faef33c910f1090c633cfeefcda62144f7177b
DispotoBrett <dispotobrett@gmail.com>
2019-03-21 13:41:38 -0700

    updated UI documentation

--- src/edu/sjsu/cs/cs151/minesweeper/view/Explodable.java
+++ src/edu/sjsu/cs/cs151/minesweeper/view/Explodable.java
@@ -1,6 +1,15 @@
 package edu.sjsu.cs.cs151.minesweeper.view;
 
+/**
+ * An implementing class has the ability to, graphically or otherwise, explode.
+ * @author JordanConragan
+ * @author BrettDispoto
+ * @author PatrickSilvestre
+ */
 public interface Explodable
 {
+	/**
+	 * Initiates and carry's out the explosion.
+	 */
 	public void explode();
 }
--- src/edu/sjsu/cs/cs151/minesweeper/view/Explosion.java
+++ src/edu/sjsu/cs/cs151/minesweeper/view/Explosion.java
@@ -5,67 +5,65 @@ import java.awt.geom.*;
 import java.awt.*;
 import java.awt.Color;
 
+/**
+ * Draws an explosion in a frame (swing).
+ *
+ * @author JordanConragan
+ * @author BrettDispoto
+ * @author PatrickSilvestre
+ */
 public class Explosion extends JComponent implements Explodable
 {
-	private static final long serialVersionUID = 1L;
-	private int width;
-	private int height;
-	private int x, y;
-	private int fadeCount;
-	private int count;
-	private boolean fade;
-	
+	//----------------Public Interface-------------------------
+
+	/**
+	 * Creates a new explosion instance.
+	 * @param x the x-coordinate of the upper left-hand corner
+	 * 					of the bounding rectangle of the explosion.
+	 * @param y the y-coordinate of the upper left-hand corner 
+	 * 					of the bounding rectangle of the explosion.
+	 */
 	public Explosion(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
-		width = 10;
-		height = 10;
-		count = 0;
-		fadeCount = 255;
-		fade = false;
+		width  = 0;
+		height = 0;
+		count  = 0;
+		fadeCount = RGB_RED_INIT;
 	}
 
+	/**
+	 * Initiates the explosion animation.
+	 */
 	public void explode()
 	{
-		if(count < 10)
-		{
-			if(width % 2 == 0)
-			{
-				x--;
-				y--;
-			}
-			height++; 
-			width++; 
-			count++;
-		}
-		if(count > 80)
-		{
-			fade = true;
-			if(fadeCount > 0)
-				fadeCount--;
-		}
-		else
-		{
-			int delta = (int) ( 1.0/1000 * Math.pow(count, 3) );
-			height += delta * 2;
-			width += delta * 2;
-			x -= delta;
-			y -= delta;
-			count++;
-		}
+		if(fadeCount > 0)
+			fadeCount--;
+	
+		int delta = (int) ( 1.0/1000 * Math.pow(count, 3) );
+		height += delta * 2;
+		width += delta * 2;
+		x -= delta;
+		y -= delta;
+		count++;
 	}
 
+	/**
+	 * Paints the explosion onto the graphics context.
+	 * @param g the graphics context.
+	 */
 	public void paint(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
 		Ellipse2D.Double explosion = new Ellipse2D.Double(x, y, width, height);
 
-		if(!fade)
-			g2.setColor(Color.RED);
-		else
-			g2.setColor(new Color(fadeCount, 0, 0));
-	
+		g2.setColor(new Color(fadeCount, 0, 0));
 		g2.fill(explosion);
 	}	
+	
+	//----------------Private Methods/Fields----------------------
+	private int x, y, height, width, fadeCount, count;
+	private static final int RGB_RED_INIT = 255;
+	private static final long serialVersionUID = 1L;
 }
