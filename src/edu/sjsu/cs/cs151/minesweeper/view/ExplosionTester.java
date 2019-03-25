858bab88c8f8cd64495769fc7860d61e1af2465e
Brett Dispoto <35391361+DispotoBrett@users.noreply.github.com>
2019-03-24 19:34:59 -0700

    made change to window location

--- src/edu/sjsu/cs/cs151/minesweeper/view/ExplosionTester.java
+++ src/edu/sjsu/cs/cs151/minesweeper/view/ExplosionTester.java
@@ -14,12 +14,13 @@ public class ExplosionTester
 	public static void main(String[] args)
 	{
 		JFrame frame = new JFrame("ExplosionTester");
-
 		Explosion explosion = new Explosion(EXPLOSION_X, EXPLOSION_Y, WIDTH, HEIGHT);
-		frame.add(explosion);
 
+		frame.setResizable(false);
+		frame.add(explosion);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		
 		Timer t = new Timer(DELAY, event ->
