53389f96773d8e5da59652730ead780b92030630
JVP15 <jordanvonpordan@gmail.com>
2019-04-06 13:36:01 -0700

    Added comments to View

--- src/edu/sjsu/cs/cs151/minesweeper/view/View.java
+++ src/edu/sjsu/cs/cs151/minesweeper/view/View.java
@@ -16,14 +16,18 @@ import javax.swing.border.*;
 
 public class View
 {
-	public static final int SPACE_BETWEEN_TILES = 5;
 	public static final int RIGHT_CLICK = 1;
 	public static final int LEFT_CLICK = 2;
 	
-	public View(int row, int col)
+	/**
+	 * Constructor for View
+	 * @param rows the number of rows of tiles that the View will display
+	 * @param cols the number of columns of tiles that the View will display
+	 */
+	public View(int rows, int cols)
 	{
-		rows = row;
-		columns = col;
+		this.rows = rows;
+		this.columns = cols;
 		messageQueue = new LinkedList<>();
 		initializeFrame();
 	}
@@ -45,13 +49,18 @@ public class View
 	private int columns;
 	private Queue<int[]> messageQueue;
 	
+	
+	/**
+	 * Creates frame, creates the panel, and fills the panel with buttons
+	 * Created to keep the constructor relatively clear
+	 */
 	private void initializeFrame()
 	{
 		frame = new JFrame("Minesweeper");
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(rows, columns));
 		
-		
+		// Fills the panel with buttons
 		for(int i = 0; i < rows; i++)
 		{
 			for(int j = 0; j < columns; j++)
@@ -59,30 +68,32 @@ public class View
 				TileIcon tile = new TileIcon(false, false, i, j);
 				JButton button = new JButton(tile);
 				
+				// Upon being clicked, the button will send a message to messageQueue that contains the location of the button and the type of click
 				button.addMouseListener( new MouseAdapter() 
 				{
-					public void MouseClicked(MouseEvent e)
+					public void MouseClicked(MouseEvent e) 
 					{
-						int row = ((TileIcon) button.getIcon()).getRow();
-						int col = ((TileIcon) button.getIcon()).getCol();
+						int row = ((TileIcon) button.getIcon()).getRow(); // The row of the button that was clicked
+						int col = ((TileIcon) button.getIcon()).getCol(); // The column of the button that was clicked
+						
 						if(SwingUtilities.isLeftMouseButton(e)) 
 							messageQueue.add(new int[] {row, col, LEFT_CLICK});
 						else if(SwingUtilities.isRightMouseButton(e)) 
 							messageQueue.add(new int[] {row, col, RIGHT_CLICK});
 					}	
 				});
+				
 				button.setPreferredSize(new Dimension(tile.getIconWidth(), tile.getIconHeight()));	
 				button.setBorder(new BevelBorder(BevelBorder.RAISED));
 				panel.add(button);
 			}
 		}
+		
 		frame.add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setSize(400,400);
 		frame.pack();
-		
-		
 		frame.setVisible(true);
 	}
 }
