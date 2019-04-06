d371074b302881d52bf6367dac3b7566eeddf6c1
Brett Dispoto <35391361+DispotoBrett@users.noreply.github.com>
2019-04-06 14:01:35 -0700

    added tileIcon documentation

--- src/edu/sjsu/cs/cs151/minesweeper/view/TileIcon.java
+++ src/edu/sjsu/cs/cs151/minesweeper/view/TileIcon.java
@@ -14,23 +14,34 @@ import javax.swing.Icon;
 public final class TileIcon implements Icon
 {
 	//----------------Public Interface-------------------------
-	
+
+	/**
+	 * Constructs a new TileIcon.
+	 * @param revealed indicates if the tile has been revealed
+	 * @param flagged indicates if the tile has been flagged
+	 * @param row the row of the tile in a board
+	 * @param col the column of the tile in a board
+	 */
 	public TileIcon(boolean revealed, boolean flagged, int row, int col)
 	{
-		this.width 	= 30;
-		this.height = 30;
 		this.revealed = revealed;
 		this.flagged  = flagged;
 		this.row = row;
 		this.col = col;
-		
 		color = revealed ? Color.WHITE : Color.LIGHT_GRAY;
 	}
 	
+	/**
+	 * Paints the Icon.
+	 * @param c the component being painted on
+	 * @param g the graphics context
+	 * @param x the x-coordinate of the bounding rectangle
+	 * @param y the y-coordinate of the bounding rectangle
+	 */
 	public void paintIcon(Component c, Graphics g, int x, int y)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		Rectangle rec = new Rectangle(width, height);
+		Rectangle rec = new Rectangle(WIDTH, HEIGHT);
 
 
 		g2.setColor(color);
@@ -48,31 +59,55 @@ public final class TileIcon implements Icon
 		}
 	}	
 
+	/**
+	 * Gets the icon's width.
+	 * @return the icon's width.
+	 */
 	public int getIconWidth()
 	{
-		return width;
+		return WIDTH;
 	}
 
+	/**
+	 * Gets the icon's height.
+	 * @return the icon's height
+	 */
 	public int getIconHeight()
 	{
-		return height;
+		return HEIGHT;
 	}
 
+	/**
+	 * Tells if the tile has been revealed
+	 * @return boolean indicating if the tile has been revealed
+	 */
 	public boolean isRevealed()
 	{
 		return revealed;
 	}
 	
+	/**
+	 * Tells if the tile has been flagged
+	 * @return boolean indicating if the tile has been flagged
+	 */
 	public boolean isFlagged()
 	{
 		return flagged;
 	}
 	
+	/**
+	 * Gets the tile's row in a Board.
+	 * @return the icon's row
+	 */
 	public int getRow()
 	{
 		return row;
 	}
 	
+	/**
+	 * Gets the tile's column in a Board.
+	 * @return the icon's column
+	 */
 	public int getCol()
 	{
 		return col;
@@ -80,7 +115,7 @@ public final class TileIcon implements Icon
 	
 	//----------------Private Methods/Fields----------------------
 
-	private int width, height;
+	private static final int WIDTH = 25, HEIGHT = 25;
 	private Color color;
 	private boolean revealed;
 	private boolean flagged;
