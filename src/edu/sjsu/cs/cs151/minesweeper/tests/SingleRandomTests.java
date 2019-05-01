9ed6af379428e3957afc447641c987d490b9b82a
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-05-01 00:08:24 -0700

    Add initial SingleRandom test

--- src/edu/sjsu/cs/cs151/minesweeper/tests/SingleRandomTests.java
+++ src/edu/sjsu/cs/cs151/minesweeper/tests/SingleRandomTests.java
@@ -1,6 +1,19 @@
 package edu.sjsu.cs.cs151.minesweeper.tests;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+import edu.sjsu.cs.cs151.minesweeper.model.SingleRandom;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SingleRandomTests
 {
-	//TODO
+	@Test
+	public void ensureOneSingleRandomInstanceTest()
+	{
+		SingleRandom aRandomNumberGenerator = SingleRandom.getInstance();
+		SingleRandom anotherRandomNumberGenerator = SingleRandom.getInstance();
+
+		assertEquals(aRandomNumberGenerator, anotherRandomNumberGenerator);
+	}
 }
